6cb8d7dcf6f431a518c3dd4d96a02d41692a0cdd
ligang <endeavor0@126.com>
2021-07-14 15:51:40 +0800

    分组，持久化

---
+++ cloud2020/cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/StreamMQMain8802.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * DATE: 2021/6/30
+ * Author: ligang
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+            SpringApplication.run(StreamMQMain8802.class,args);
+        }
+}
